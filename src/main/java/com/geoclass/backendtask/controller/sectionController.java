1a9ce3fee4b562366cfa5d5a14855fd4c1a23a00
faizantahir <faizanm88@yahoo.com>
2023-10-14 17:39:30 +0300

    import statement removed

--- src/main/java/com/geoclass/backendtask/controller/sectionController.java
+++ src/main/java/com/geoclass/backendtask/controller/sectionController.java
@@ -1,6 +1,5 @@
 package com.geoclass.backendtask.controller;
 
-import com.geoclass.backendtask.dto.SectionDTO;
 import com.geoclass.backendtask.entities.sectionEntity;
 import com.geoclass.backendtask.service.sectionService;
 import org.springframework.beans.factory.annotation.Autowired;
